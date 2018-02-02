e3c19942a078a692bb13877b4cd49ec6728d12d6
Anna Hadjitofi <ach17@sprite27.doc.ic.ac.uk>
2018-02-02 23:06:29 +0000

    mess

--- df_quad.pl
+++ df_quad.pl
@@ -20,11 +20,23 @@ base(a3, 0.5).
 base(b, 0.5).
 
 
-
+/*
 comb_func(V0, Va, Vs, C):-
 	(Va >= Vs -> C is (V0 - (V0 * abs((Vs - Va)))) ;
 	 C is (V0 + ((1 - V0) * (abs((Vs - Va)))))).
+*/
 
+comb_func(V0, Va, Vs, C):-
+	Va =< Vs,
+	%print(Va),nl,
+	%print(Vs),nl,
+	C is (V0 - (V0 * abs((Vs - Va)))).
+
+comb_func(V0, Va, Vs, C):-
+	Va > Vs,
+	C is (V0 + ((1 - V0) * (abs((Vs - Va))))).
+
+/*
 aggreg_func(0, [], 0).
 
 aggreg_func(1, [Score], Score).
@@ -41,17 +53,115 @@ aggreg_func(N, [Score1, Score2|T], Score):-
         aggreg_func(N1,NewList, Temp),
 	Score is Temp.
 
+*/
+
 base_func(V1, V2, NewScore):-
-	NewScore = V1 + (1-V1)*V2.
+	NewScore is (V1 + ((1 - V1)* V2)).
 
 strength(Arg, FinalScore):-
 	base(Arg, BS),
-	findall(Att_Score, (argument(X), attacks(X,Arg), base(Arg, Att_Score)), Att_Scores),
-	length(Att_Scores, Attck_length),
-	findall(Supp_Score, (argument(X), supports(X,Arg), base(Arg, Supp_Score)), Supp_Scores),
-	length(Supp_Scores, Supp_length),
-	aggreg_func(Attck_length, Att_Scores, Attack_total_score),
-	aggreg_func(Supp_length, Supp_Scores, Supp_total_score),
-	comb_func(BS, Attack_total_score, Supp_total_score, FinalScore),
-	print(FinalScore).
+	recurse(Arg, Attackers_Score),
+	print(fdsa), print(Attackers_Score),
+	comb_func(BS, Attackers_Score, 0, FinalScore).
 
+recurse(Arg, Score):-
+	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
+	length(Attackers, Attck_length),
+	Attck_length == 0,
+	Score is 0.
+
+recurse(Arg, Score):-
+	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
+	length(Attackers, Attck_length),
+	Attck_length == 1,
+	member(A, Attackers),
+	base(A, Score).
+
+recurse(Arg, Score):-
+	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
+	length(Attackers, Attck_length),
+	Attck_length == 2,
+	[Attacker1, Attacker2] = Attackers,
+	base(Attacker1, Score1),
+	base(Attacker2, Score2),
+	base_func(Score1, Score2, Score),	
+	member(A, Attackers).
+
+
+recurse(Arg, Score):-
+	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
+	length(Attackers, Attck_length),
+	Attck_length > 2,
+	member(A, Attackers),
+	print(score), print(Score),nl,
+	N > 2,
+	N1 is N - 1,
+	base_func(Score1, Score2, NewScore),
+	append([NewScore], T, NewList),
+        aggreg_func(N1,NewList, Temp),
+	Score is Temp.
+	recurse(A, AttackScore),
+	print(Attackers_Score),nl,
+	aggreg_func(Attck_length, Attackers_Score, AttackScore).
+
+
+/*
+
+recurse(Arg, Score):-
+	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
+	length(Attackers, Attck_length),
+	Attck_length > 2,
+	member(A, Attackers),
+	print(score), print(Score),nl,
+	N > 2,
+	N1 is N - 1,
+	base_func(Score1, Score2, NewScore),
+	append([NewScore], T, NewList),
+        aggreg_func(N1,NewList, Temp),
+	Score is Temp.
+	recurse(A, AttackScore),
+	print(Attackers_Score),nl,
+	aggreg_func(Attck_length, Attackers_Score, AttackScore).
+
+
+*/
+
+
+
+/*
+strength(Arg, AS):-
+	base(Arg, BS),
+	findall(Att, (argument(Att), attacks(Att,Arg), base(Arg, Att_Score)), Attackers),
+	findall(Att_Score, (argument(Att), attacks(Att,Arg), base(Arg, Att_Score)), Attackers_Score),
+	length(Attackers_Score, Att_length),
+	Att_length == 0,
+	aggreg_func(Att_length, Attackers, AS).
+*/
+
+
+
+
+/*
+strength(Arg, FinalScore):-
+	base(Arg, BS),
+	findall(Att, (argument(Att), attacks(Att,Arg), base(Arg, Att_Score)), Attackers),
+	print(Attackers),nl,
+	findall(Att_Score, (argument(Att), attacks(Att,Arg), base(Arg, Att_Score)), Attackers_Score),
+	length(Attackers, Attck_length),
+	Attck_length > 0,
+	member(A, Attackers),
+	strength(A, AttackScore),
+	aggreg_func(Attck_length, Attackers_Scores, Attack_total_score),
+
+	findall(Supp, (argument(Supp), supports(Supp,Arg), base(Arg, Supp_Score)), Supporters),
+	findall(Supp_Score, (argument(Supp), supports(Supp,Arg), base(Arg, Supp_Score)), Supporters_Score),
+	length(Supporters, Supp_length),
+	Supp_length > 0,
+	member(S, Supporters),
+	strength(S, Supporters),
+	aggreg_func(Supp_length, Supporters_Scores, Supp_total_score),
+		
+	comb_func(BS, Attack_total_score, Supp_total_score, FinalScore).
+
+	
+*/
