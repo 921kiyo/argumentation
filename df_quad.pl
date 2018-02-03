7b49e6300b7694ecb0fc7025995456cba3aa8698
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2018-02-03 19:04:47 +0000

    fix multiple returns bug, but combination of multiple attacks and supports break the program....

--- df_quad.pl
+++ df_quad.pl
@@ -6,14 +6,11 @@ argument(a3).
 argument(a4).
 argument(a5).
 argument(a6).
-% argument(b).
+argument(a7).
 
 % relationships
 
-% attacks(a2, a1).
-% attacks(b, a1).
-%
-% supports(a3, a1).
+supports(a7, a2).
 
 attacks(a5,a3).
 attacks(a6,a3).
@@ -29,8 +26,20 @@ base(a3, 0.5).
 base(a4, 0.5).
 base(a5, 0.5).
 base(a6, 0.5).
+base(a7, 0.5).
 % base(a5, 0.5).
 % base(b, 0.5).
+
+
+/*
+TODO: Check edge cases
+
+1. If no attacks/2 is provided
+2. If no supports/2 is provided
+3. When there are more supporters than attackers
+4. Haven't tested different base score
+5. Very complicated trees (we can do this using http://www.arganddec.com/)
+*/
 
 
 /*
@@ -43,61 +52,74 @@ comb_func(V0, Va, Vs, C):-
 	Va >= Vs,
 	C is (V0 - (V0 * abs((Vs - Va)))).
 
-
 comb_func(V0, Va, Vs, C):-
 	Va < Vs,
 	C is (V0 + ((1 - V0) * (abs((Vs - Va))))).
 
-/*
-aggreg_func(0, [], 0).
-
-aggreg_func(1, [Score], Score).
-
-aggreg_func(2, [Score1, Score2], Score):-
-	base_func(Score1, Score2, Score).
-
-
-aggreg_func(N, [Score1, Score2|T], Score):-
-	N > 2,
-	N1 is N - 1,
-	base_func(Score1, Score2, NewScore),
-	append([NewScore], T, NewList),
-        aggreg_func(N1,NewList, Temp),
-	Score is Temp.
-
-*/
-
 base_func(V1, V2, NewScore):-
 	NewScore is (V1 + ((1 - V1)* V2)).
 
+
+% This works when there is only supporters
+
 strength(Arg, BS):-
-  % I think this negation is causing multiple answers issue....
 	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
 	length(Attackers, 0),
-	base(Arg, BS),
-	argument(Att).
-
+	base(Arg, BS).
 strength(Arg, TotalScore):-
-  % This is causing multiple answers
-	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
-	length(Attackers, 1),
-	print(Arg),nl,
-	print(Attackers),nl,
-	strength_aggregation(Arg, Attackers, Score),
-	print(Score),nl,
-	base(Arg, BS),
-	comb_func(BS, Score, 0, TotalScore).
-
-strength(Arg, TotalScore):-
-  % This is causing multiple answers
-	attacks(A, Arg),
 	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
 	length(Attackers, Att_len),
-	Att_len > 1,
+	Att_len > 0,
 	strength_aggregation(Arg, Attackers, Score),
 	base(Arg, BS),
-	[Att1, Att2] = Attackers,
 	comb_func(BS, Score, 0, TotalScore).
+
+% % When Arg has no child
+% strength(Arg, BS):-
+% 	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
+% 	length(Attackers, 0),
+% 	findall(Supp, (argument(Supp), supports(Supp,Arg)), Supporters),
+% 	length(Supporters, 0),
+% 	base(Arg, BS).
+%
+% % When Arg has only supporter children and no attackers
+% strength(Arg, TotalScore):-
+% 	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
+% 	length(Attackers, 0),
+%
+% 	findall(Supp, (argument(Supp), supports(Supp,Arg)), Supporters),
+% 	length(Supporters, Supp_len),
+% 	Supp_len > 0,
+% 	strength_aggregation(Arg, Supporters, SuppScore),
+% 	base(Arg, BS),
+% 	comb_func(BS, 0, SuppScore, TotalScore).
+%
+% % When Arg has only attacker children and no supporters
+% strength(Arg, TotalScore):-
+% 	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
+% 	length(Attackers, Att_len),
+% 	Att_len > 0,
+% 	strength_aggregation(Arg, Attackers, AttScore),
+%
+% 	findall(Supp, (argument(Supp), supports(Supp,Arg)), Supporters),
+% 	length(Supporters, 0),
+% 	base(Arg, BS),
+% 	comb_func(BS, AttScore, 0, TotalScore).
+%
+% % When Arg has both attacker and supporter children
+% strength(Arg, TotalScore):-
+% 	findall(Att, (argument(Att), attacks(Att,Arg)), Attackers),
+% 	length(Attackers, Att_len),
+% 	Att_len > 0,
+% 	strength_aggregation(Arg, Attackers, AttScore),
+% 	findall(Supp, (argument(Supp), supports(Supp,Arg)), Supporters),
+% 	length(Supporters, Supp_len),
+% 	Supp_len > 0,
+% 	strength_aggregation(Arg, Supporters, SuppScore),
+% 	% print(attscore), print(AttScore),nl,
+% 	% print(suppscore), print(SuppScore),nl,
+% 	base(Arg, BS),
+% 	comb_func(BS, AttScore, Supp, TotalScore).
 
 strength_aggregation(Arg, Attackers, Score):- strength_aggregation(Arg, Attackers, Score, []).
 
