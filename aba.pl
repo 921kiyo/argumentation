2f8cd078d34ae1f16f85ef5f2317392e5d8b98bd
Anna Hadjitofi <ach17@point26.doc.ic.ac.uk>
2018-02-08 21:25:50 +0000

    Add comment

--- aba.pl
+++ aba.pl
@@ -10,9 +10,15 @@ argument((C, [X|Y])):-
 
 argument((C, [X])):-
 	myAsm(X),
-	\+ myRule(C, [X|Y]),
+	\+ myRule(C, [X|_]),
 	X = C.
 
 argument((C, [])):-
 	myRule(C, []).
 
+attacks((C1, X1), (C2, X2)):-
+	argument((C1, X1)),
+	myRule(C2, [X|Y]),
+	Attacker = X,
+	contrary(Attacker, C1).
+	
