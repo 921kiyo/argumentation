93a836e47c4f3738a982f1fedd01150a5e156625
Kiyohito Kunii (Kiyo) <kiyo921@hotmail.co.jp>
2018-02-10 14:07:59 +0000

    argument working now

--- aba.pl
+++ aba.pl
@@ -5,6 +5,16 @@ contrary(a, p).
 myRule(p, [b]).
 myRule(p, []).
 
+
+% LabTS test 3
+% argument((z, X)).
+% myAsm(a).
+% myAsm(b).
+% myAsm(c).
+% myRule(x,[]).
+% myRule(y,[b]).
+% myRule(z,[a, p]).
+
 % Examples in the paper
 % myAsm(a).
 % myAsm(b).
@@ -15,24 +25,66 @@ myRule(p, []).
 % myRule(p, [q,a]).
 % myRule(q, []).
 % myRule(r, [b,c]).
-argument((C, [])):-
-	myRule(C, []).
+% argument((C, [])):-
+% 	myRule(C, []).
 
+% If C is an assumption, return itself
 argument((C, [C])):-
 	myAsm(C).
 
-% argument((C, Res)):-
-%
-% 	myRule(C, L),
-% 	findall(X, (myRule(C, List), member(X, List)), Lis),
-% 	combs(Lis, Res).
+% If C is NOT an assumption, check all supports
+argument((C, Res)):-
+	\+(myAsm(C)),
+	myRule(C, List),
+	length(List, Len),
+	Len > 0,
+	check_support(List),
+	get_assumption(List, Res).
 
-combs([],[]).
+get_assumption([], []).
+get_assumption([H|T1], [H|T2]):-
+	myAsm(H),
+	get_assumption(T1,T2).
 
-combs([H|T],[H|T2]) :-
-    combs(T,T2).
-combs([_|T],T2) :-
-    combs(T,T2).
+get_assumption([H1|T1], T2):-
+	\+(myAsm(H1)),
+	member(H1, List),
+	get_assumption(C, T1,T2).
+
+check_support([]).
+
+check_support([H|T]):-
+	myAsm(H),
+	check_support(T).
+
+check_support([H|T]):-
+	\+(myAsm(H)),
+	myRule(H, List),
+	check_support(List),
+	check_support(T).
+
+% If C is not an assumption, and empty support, return empty set.
+argument((C, [])):-
+	\+(myAsm(C)),
+	myRule(C, List),
+	length(List, 0).
+
+% % X has to be instantiated
+argument((C, X)):-
+	\+(not_inst(X)),
+	check_support2(X, C),
+	myRule(C, X).
+
+check_support2([], C).
+check_support2([H|T], C):-
+	myRule(C, List),
+	member(H, List),
+	check_support2(T, C).
+
+not_inst(Var):-
+  \+(\+(Var=0)),
+  \+(\+(Var=1)).
+
 
 
 attacks((C1, X1), (C2, X2)):-
@@ -43,30 +95,3 @@ attacks((C1, X1), (C2, X2)):-
 	argument(X2, [C2]).
 	% myRule(C2, [Attacker|Y]),
 	% contrary(Attacker, C1).
-
-
-% argument((C, List)):-
-% 	length(List, Len),
-% 	Len > 1,
-% 	% check_argument(List),
-% 	findall(X, (myAsm(X), member(X, List)), L),
-% 	print(L),nl,
-% 	myRule(C, List).
-%
-% % check_argument([]).
-% % check_argument([H|T]):-
-% % 	myAsm(H),
-% % 	check_argument(T).
-%
-% % When there is only one assumption
-% argument((C, [X])):-
-% 	myAsm(X),
-% 	\+ myRule(C, [X|_]),
-% 	X = C.
-%
-% % If C itself is an assumption
-% argument((C, [C])):-
-% 	myAsm(C).
-%
-% argument((C, [])):-
-% 	myRule(C, []).
