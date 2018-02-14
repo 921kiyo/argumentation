95284b5ff02524d1cd65fbc67551be967b9f7d62
Anna Hadjitofi <ach17@line17.doc.ic.ac.uk>
2018-02-14 14:28:59 +0000

    Added one possible proof for ann gets 6, boris gets none - need to check

--- temp.pl
+++ temp.pl
@@ -40,3 +40,30 @@ X = (not_sports(b),{overweight(b)}) ? ;
 % 16. X = (not_free6(b),[sports(b)]) ? ;
 % 17. X = (not_free8(a),[child(a)]) ? ;
 18. X = (not_sports(b),[overweight(b)]) ? ;
+
+
+
+
+
+
+
+% ann gets 6, boris gets none - think its okay(?)
+
+%1. X = (free6(a),[free6(a)]) ? ;
+2. X = (free8(a),[free8(a)]) ? ;
+%3. X = (free6(b),[free6(b)]) ? ;
+%4. X = (free8(b),[free8(b)]) ? ;
+5. X = (sports(b),[sports(b)]) ? ;
+%6. X = (child(a),[child(a)]) ? ;
+%7. X = (overweight(b),[overweight(b)]) ? ;
+8. X = (not_get6(a),[not_get6(a)]) ? ;
+%9. X = (not_get8(a),[not_get8(a)]) ? ;
+%10. X = (not_get6(b),[not_get6(b)]) ? ;
+%11. X = (not_get8(b),[not_get8(b)]) ? ;
+%12. X = (get6(a),[free6(a),not_get6(b),not_get8(a)]) ? ;
+13. X = (get6(b),[free6(b),not_get6(a),not_get8(b)]) ? ;
+14. X = (get8(b),[free8(b),not_get8(a),not_get6(b)]) ? ;
+15. X = (get8(a),[free8(a),not_get8(b),not_get6(a)]) ? ;
+16. X = (not_free6(b),[sports(b)]) ? ;
+%17. X = (not_free8(a),[child(a)]) ? ;
+%18. X = (not_sports(b),[overweight(b)]) ? ;
